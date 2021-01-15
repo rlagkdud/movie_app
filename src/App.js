bd2f1e213f7ea1b18fc9f3049a61eef1aa41a572
rlagkdud <ha0936861@gmail.com>
2021-01-15 17:14:06 +0900

    class component and state

--- src/App.js
+++ src/App.js
@@ -1,61 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const foodILike = [
-  {
-    id:1,
-    name:"kimchi",
-    img:"http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
-  },
-  {
-    id:2,
-    name:"samgiopsal",
-    img:"https://www.gildedgingerbread.com/wp-content/uploads/2017/08/Samgyeopsal-1.jpg",
-    rating: 4.9
-  },
-  {
-    id:3,
-    name:"dongas",
-    img:"https://s3-media4.fl.yelpcdn.com/bphoto/anesu6IAyAj2d6r19eFqEg/o.jpg",
-    rating:3.2
-  },
-  {
-    id:4,
-    name:"icecream",
-    img:"https://laurenslatest.com/wp-content/uploads/2020/08/vanilla-ice-cream-5-copy.jpg",
-    rating:2.2
-  },
-  {
-    id:5,
-    name:"chocolate",
-    img:"https://cdn-image.foodandwine.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/fwx-melted-chocolate.jpg?itok=9NqasBcf",
-    rating:4.3
-  }
-]
-
-function Food({name,picture,rating}){
-  return <div>
-    <h2>I like {name}.</h2> 
-    <h4>{rating} / 5</h4>
-    <img src={picture} alt={name}/>
-  </div>
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number
-}
-
-function App() {
-  return (
-  <div>
-    <h1>Hello</h1>
-    {foodILike.map(dish=>
-        <Food key={dish.id} name={dish.name} picture={dish.img} rating={dish.rating}/>
-      )}
-    </div>
+class App extends React.Component{
+  state = {
+    count: 0
+  };
+  add=()=>{
+    console.log("add");
+  };
+  minus=()=>{
+    console.log("minus");
+  };
+  render(){
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>add</button>
+        <button onClick={this.minus}>minus</button>
+      </div>
     );
+  }
 }
-
 export default App;
