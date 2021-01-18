0d97a85abbe0203e88f34ebbe9fd29a4cbcbf9dd
rlagkdud <ha0936861@gmail.com>
2021-01-18 17:29:38 +0900

    fix html and add css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component{
   state = {
@@ -22,17 +23,25 @@ class App extends React.Component{
   render(){
     const { isLoading ,movies} = this.state;
     return (
-      <div>{isLoading ? "Loading" : movies.map(movie => (
-        <Movie 
-          key={movie.id}
-          id={movie.id} 
-          year={movie.year} 
-          title={movie.title} 
-          summary={movie.summary} 
-          poster={movie.medium_cover_image}
-        />
-      ))}
-      </div>
+      <section class="container">
+        {isLoading ? 
+        <div>
+          <span>Loading...</span>
+        </div> :
+        <div class="movies">
+          { movies.map(movie => (
+            <Movie 
+              key={movie.id}
+              id={movie.id} 
+              year={movie.year} 
+              title={movie.title} 
+              summary={movie.summary} 
+              poster={movie.medium_cover_image}
+            />
+          ))}
+        </div>
+        }
+      </section>
     );
   }
 }
